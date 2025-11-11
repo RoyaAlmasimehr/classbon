529bd3a9c66b131ecf5d10ec7fd1ea2cdccb582e
RoyaAlmasimehr <almasimehrroya@gmail.com>
2025-11-12 00:10:35 +0330

    feat footer

---
+++ src/app/_components/testimonial/testimonial.types.ts
@@ -0,0 +1,11 @@
+import { Testimonial } from "@/types/testimonial.type"
+
+
+export type TestimonialProps={
+    testimonial:Testimonial
+
+}
+
+export type TestimonialListProps = {
+  testimonials: Testimonial[];
+};
