4cf44cab7d5e788c77a89037d901b7d7cba51922
RoyaAlmasimehr <almasimehrroya@gmail.com>
2025-12-09 15:29:42 +0330

    add rating

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,3 +1,5 @@
+
+import { Rating } from "@/app/_components/rating/rating";
 import { API_URL } from "@/configs/global";
 import type { CourseDetails } from "@/types/course-details.interface";
 export async function generateStaticParams() {
@@ -22,10 +24,54 @@ export default async function CourseDetails({
   params: { slug: string };
 }) {
   const { slug } = params;
-  const courseData = await getCourse(slug);
+    const course = await getCourse(slug);
+        // const faqs: AccordionType[] = course.frequentlyAskedQuestions.map(
+        //   (faq) => ({
+        //     id: faq.id,
+        //     title: faq.question,
+        //     content: faq.answer,
+        //   })
+        // );
+
+        // const tabs: Tab[] = [
+        //   {
+        //     label: "مشخصات دوره",
+        //     content: course.description,
+        //   },
+        //   {
+        //     label: "دیدگاه‌ها و پرسش",
+        //     content: <CourseComments />,
+        //   },
+        //   {
+        //     label: "سوالات متداول",
+        //     content: <Accordion data={faqs} />,
+        //   },
+        // ];
+
   return (
-    <div className="text-5xl flex justify-center items-center w-full">
-      <h1>{courseData.title}</h1>
-    </div>
+    
+            <div className="container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+            <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
+            <div className="col-span-10 xl:col-span-7">
+                <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+                    {course.title}
+                </h1>
+                <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+                    {course.subTitle}
+                </h2>
+
+                <div className=" mt-5">Video Player Component</div>
+            </div>
+            <div className="col-span-10 xl:col-span-3">
+                {/* <CourseAside {...course} /> */}
+            </div>
+            <div className="col-span-10 xl:col-span-6">
+                {/* <Tabs tabs={tabs}/> */}
+                <Rating  rate={3} variant="info"/>
+            </div>
+            <div className="col-span-10 xl:col-span-4 bg-warning"></div>
+        </div>
+    
+
   );
 }
