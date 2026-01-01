5e03a241ff7da235986165f779ab1ac9d2b6e241
RoyaAlmasimehr <almasimehrroya@gmail.com>
2026-01-02 01:42:49 +0330

    add textplaceholder

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -1,26 +1,25 @@
-'use client'
-
+"use client";
 import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_api/get-comments";
 import { Comment } from "@/app/_components/comment";
+import { TextPlaceholder } from "@/app/_components/placeholders";
 
+const CourseComments = () => {
+  const { slug } = useParams();
+  const { data: comments, isLoading } = useCourseComments({
+    params: {
+      slug: slug as string,
+      page: 1,
+    },
+  });
 
-
-const CourseComments =()=>{
-const {slug}=useParams();
-const{data:comments}=useCourseComments({
-    params:{
-        slug:slug as string,
-        page:1
-
-    }
-})    
-return(
-        <>
-        {comments?.data.map((comment)=>(
-<Comment key={`comment-${comment.id}`} {...comment} variant='info'/>
-        ))}
-        </>
-    )
-}
- export default CourseComments;
+  return (
+    <>
+      {comments?.data.map((comment) => (
+        <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
+      ))}
+      {isLoading && <TextPlaceholder />}
+    </>
+  );
+};
+export default CourseComments;
