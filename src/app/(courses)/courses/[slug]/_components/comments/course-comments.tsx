3fcffce0ee7a43f0f331b092946fd15976083402
RoyaAlmasimehr <almasimehrroya@gmail.com>
2026-01-02 02:13:02 +0330

    add infintescroll

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -3,10 +3,11 @@ import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_api/get-comments";
 import { Comment } from "@/app/_components/comment";
 import { TextPlaceholder } from "@/app/_components/placeholders";
+import { Fragment } from "react";
 
 const CourseComments = () => {
   const { slug } = useParams();
-  const { data: comments, isLoading } = useCourseComments({
+  const { data: comments, error,isFetchingNextPage,fetchNextPage,hasNextPage,refetch} = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
@@ -15,10 +16,19 @@ const CourseComments = () => {
 
   return (
     <>
-      {comments?.data.map((comment) => (
-        <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
+      {comments?.pages.map((currentPage) => (
+        <Fragment key={`comment-page-${currentPage}`}>
+          {currentPage.data.map((comment) => (
+            <Comment
+              key={`comment-${comment.id}`}
+              {...comment}
+              variant="info"
+            />
+          ))}
+        </Fragment>
       ))}
-      {isLoading && <TextPlaceholder />}
+     
+      {/* {isF && <TextPlaceholder />} */}
     </>
   );
 };
