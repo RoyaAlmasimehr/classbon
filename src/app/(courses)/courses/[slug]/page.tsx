6ae682416fc4f9010e2eda4adc716e12621cc12e
RoyaAlmasimehr <almasimehrroya@gmail.com>
2026-01-03 15:15:30 +0330

    feat videoplayer

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -8,6 +8,8 @@ import { Accordion as AccordionType } from "@/types/accordion";
 import CourseComments from "./_components/comments/course-comments";
 import { CourseChapter } from "@/types/course-chapter.interface";
 import { CourseCurriculum } from "./_components/curriculum";
+import { VideoPlayer } from "@/app/_components/video-player";
+import Image from "next/image";
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
     res.json()
@@ -71,26 +73,43 @@ const faqs: AccordionType[] = course.frequentlyAskedQuestions.map((faq) => ({
     <div className="container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
       <div className="dark:bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
       <div className="col-span-10 xl:col-span-7">
-        <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+        <h1 className="text-center xl:text-right text-gray-200 text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
           {course.title}
         </h1>
         <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
           {course.subTitle}
         </h2>
 
-        <div className=" mt-5">Video Player Component</div>
+        <div className=" mt-5">
+          {course.videoUrl ? (
+            <VideoPlayer
+              src={course.videoUrl}
+              poster={`${API_URL}/picture/${course.coverImageId}`}
+            />
+          ) : (
+            <Image
+              src={`https://api.classbon.com/api/picture/${course.coverImageId}`}
+              alt={course.title}
+              width={550}
+              height={327}
+              className="w-full"
+            />
+          )}
+        </div>
       </div>
       <div className="col-span-10 xl:col-span-3">
         <CourseAside {...course} />
       </div>
-      <div className="col-span-10 xl:col-span-6 
-      ">
+      <div
+        className="col-span-10 xl:col-span-6 
+      "
+      >
         <Tabs tabs={tabs} />
       </div>
       <div className="col-span-10 xl:col-span-4 ">
         <div className="sticky top-5">
-<h2 className="mb-5 text-xl" >سرفصل های دوره</h2>
-<CourseCurriculum  data={courseCurriculum}/>
+          <h2 className="mb-5 text-xl">سرفصل های دوره</h2>
+          <CourseCurriculum data={courseCurriculum} />
         </div>
       </div>
     </div>
